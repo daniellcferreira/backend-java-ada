6efd08a3c922dde020583c9aaf9bd2ca0f418de1
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-06-21 10:59:49 -0300

    create lesson estrutura-dados

---
+++ estrutura-dados/supermarket/src/main/java/ada/implementation/Supermarket.java
@@ -0,0 +1,10 @@
+package ada.implementation;
+
+public interface Supermarket {
+
+    void add(final String item);
+
+    void print();
+
+    void delete(final int index);
+}
--- poo-java/src/App.java
+++ poo-java/src/App.java
@@ -1,6 +1,7 @@
 import Animais.Cachorro;
 import Animais.Gato;
 import Animais.Passaro;
+import Lojas.Petshop;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,6 +11,18 @@ public class App {
         Gato gato = new Gato("luds", "branco", 3);
 
         Passaro passaro = new Passaro("bird", "marron", 2);
+
+        Petshop petshop = new Petshop();
+
+        petshop.darBanho(passaro);
+        System.out.println(passaro.getEstadoDeEspirito());
+
+        petshop.darBanho(gato);
+        System.out.println(gato.getEstadoDeEspirito());
+
+        petshop.tosar(cachorro);
+        System.out.println(cachorro.getEstadoDeEspirito());
+
 
     }
 }
