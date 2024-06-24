aea9aeb08630c65ac9028c1d6e5d0e6bcfe01467
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-06-24 09:56:35 -0300

    create lesson queue

---
+++ estrutura-dados/estrutura-de-dados/src/main/java/ada/Main/FilaAtendimento.java
@@ -0,0 +1,29 @@
+package ada.Main;
+
+import java.util.Random;
+
+import ada.dataStructure.Queue;
+
+public class FilaAtendimento {
+
+    public static void main(String[] args) {
+        int n = 5;
+        var random = new Random();
+        int cliente = random.nextInt(101);
+        Queue queue = new Queue(cliente);
+
+        System.out.println("Chegou o cliente " + cliente);
+        // chegada dos clientes
+        for (int i = 2; i <= n; i++) {
+            cliente = random.nextInt(101);
+            System.out.println("CHegou o cliente " + cliente);
+            queue.enqueue(cliente);
+        }
+        // atendimento dos clientes
+        var node = queue.dequeue();
+        while (node != null) {
+            System.out.println("Atendido o cliente: " + node.getValue());
+            node = queue.dequeue();
+        }
+    }
+}
